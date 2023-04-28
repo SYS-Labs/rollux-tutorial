d62ff9a55b10b0a32cf998bb8bf4a756198251fb
EE127F <andrekmbf@protonmail.com>
2023-04-28 17:24:18 +0200

    added to /foundry/

--- getting-started/foundry/script/Contract.s.sol
+++ getting-started/foundry/script/Contract.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Greeter.sol";
 
-contract ContractScript is Script {
-    function setUp() public {}
+contract MyScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-    function run() public {
-        vm.broadcast();
+        Greeter greeter = new Greeter("Hello World!");
+
+        vm.stopBroadcast();
     }
 }
--- getting-started/foundry/src/Greeter.sol
+++ getting-started/foundry/src/Greeter.sol
@@ -1,14 +1,15 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.13;
 
-// import "hardhat/console.sol";
-
+//console2.sol contains patches to console.sol that allow Forge to decode traces for calls to the console, but it is not compatible with Hardhat.
+// import "forge-std/console.sol";
+import "forge-std/console2.sol";
 
 contract Greeter {
   string greeting;
 
   constructor(string memory _greeting) {
-//    console.log("Deploying a Greeter with greeting:", _greeting);
+   console.log("Deploying a Greeter with greeting:", _greeting);
     greeting = _greeting;
   }
 
@@ -17,7 +18,7 @@ contract Greeter {
   }
 
   function setGreeting(string memory _greeting) public {
-//    console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+   console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
     greeting = _greeting;
   }
 }
